593dae873d512c2078fc86bfd871138a8241815f
vitalics <vitalicset@yandex.ru>
2017-04-09 16:37:54 -0700

    re configured project. Less as preprocessor. webpack 2 with hot module replacing. Update gitignore and license

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-    ],
     declarations: [
         AppComponent,
+        HelloWorldComponent,
     ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
